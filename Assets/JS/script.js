9393a512f5289687c1f4d37a5b30a7911ed6c9f3
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-19 14:18:08 -0400

    question functionality

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,64 +1,74 @@
 //start button functionality
 var startButton = document.querySelector("#start");
 startButton.addEventListener("click", init);
-
-//variable for the questions and answers
-var questions = {
-    1: {
-        question: "What is ...?",
-        correctAnswer: "B",
+//variable for the questions
+var questions = [
+    {
+        question: "What is JavaScript?",
         answers: {
-            "A": "answer 1",
-            "B": "answer 2",
-            "C": "answer 3",
-            "D": "answer 4"
-        }
-    },
-    2: {
-        question: "What is ...?",
+            "A": "A document system",
+            "B": "A computer game",
+            "C": "A programming language",
+            "D": "An animal from the Amazon"
+        },
         correctAnswer: "C",
-        answers: {
-            "A": "answer 1",
-            "B": "answer 2",
-            "C": "answer 3",
-            "D": "answer 4"
-        }
-    }
-};
+    },
 
+];
 //calling the questions
 var currentQuestion = 0;
+
 //changing the hidden visibility on the element to switch to the questions
 function init() {
     document.getElementById("intro").classList.add("hidden");
     document.getElementById("question-content").classList.remove("hidden");
-    loadQuestion();
+    loadNextQuestion();
 }
-
 //formula to cycle through the questions
-function loadQuestion() {
-    var questionSelection = questions[currentQuestion++];
+function loadNextQuestion() {
+    var questionSelection = questions[currentQuestion];
 
-    var answers = [];
+    document.getElementById("question").innerHTML = questionSelection.question;
 
-    for (var i = 0; i <= questionSelection.answers.length; i++) {
+    for (letter in questionSelection.answers) {
+        var answerText = questionSelection.answers[letter];
         var answer = document.createElement("li");
 
-        answer.id = questionSelection.answers[i].key;
-        answer.value = questionSelection.answers[i].value;
-        answer.innerHTML = questionSelection.answers[i].value;
-
+        // <li id="A" value="A">A document system</li>
+        answer.id = letter;
+        answer.innerHTML = answerText;
+        answer.addEventListener("click", answerCheck);
         document.getElementById("answers").appendChild(answer);
     }
+}
+//click event for the answer choices
+function answerCheck(event){
+    var questionSelection = questions[currentQuestion];
+    var isCorrect = event.target.id === questionSelection.correctAnswer;
 
+    //do some stuff to the score
+
+    //tell them if it is correct or wrong
+
+    currentQuestion++;
+
+    if (questions.length === currentQuestion) {
+        stop();
+        return;
+    }
+
+    loadNextQuestion();
 }
 
-//formula to keep track of score
+function stop() {
+    //hide question
+    //ask for initials and display score
+    //show full list of scores
+    //show score
+}
 
 //timer function - must subtract when the answer is wrong
 
-//ask for initials and show score
+//reload button
 
-//show list of scores
-
-//use local storage to keep and pull high scores
+//use local storage to keep and pull high scores upon reload 
