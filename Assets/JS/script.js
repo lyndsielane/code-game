50ad5c8a00afa0897ad77c164e6db34fe0b87b20
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-19 22:02:01 -0400

    question looping and replay button

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,7 @@
 //start button functionality
 var startButton = document.querySelector("#start");
 startButton.addEventListener("click", init);
+
 //variable for the questions
 var questions = [
     {
@@ -14,18 +15,35 @@ var questions = [
         correctAnswer: "C",
     },
 
+    {
+        question: "What is the purpose of CSS?",
+        answers: {
+            "A": "To sound confusing",
+            "B": "To style a webpage",
+            "C": "To add color to a document",
+            "D": "To color something special"
+        },
+        correctAnswer: "B",
+    },
+
 ];
 //calling the questions
 var currentQuestion = 0;
 
 //changing the hidden visibility on the element to switch to the questions
 function init() {
+    currentQuestion = 0;
     document.getElementById("intro").classList.add("hidden");
     document.getElementById("question-content").classList.remove("hidden");
+    document.getElementById("results").classList.add("hidden");
     loadNextQuestion();
 }
 //formula to cycle through the questions
 function loadNextQuestion() {
+    var answers = document.getElementById("answers");
+
+    answers.innerHTML = '';
+
     var questionSelection = questions[currentQuestion];
 
     document.getElementById("question").innerHTML = questionSelection.question;
@@ -38,15 +56,15 @@ function loadNextQuestion() {
         answer.id = letter;
         answer.innerHTML = answerText;
         answer.addEventListener("click", answerCheck);
-        document.getElementById("answers").appendChild(answer);
+        answers.appendChild(answer);
     }
 }
-//click event for the answer choices
+//checking the answer choices
 function answerCheck(event){
     var questionSelection = questions[currentQuestion];
     var isCorrect = event.target.id === questionSelection.correctAnswer;
 
-    //do some stuff to the score
+    //do some stuff to the timer
 
     //tell them if it is correct or wrong
 
@@ -62,13 +80,15 @@ function answerCheck(event){
 
 function stop() {
     //hide question
-    //ask for initials and display score
-    //show full list of scores
-    //show score
+    document.getElementById("question-content").classList.add("hidden");
+    document.getElementById("results").classList.remove("hidden");
+    //ask for initials and display scores
 }
 
 //timer function - must subtract when the answer is wrong
 
 //reload button
+var replayButton = document.querySelector("#replay");
+replayButton.addEventListener("click", init);
 
 //use local storage to keep and pull high scores upon reload 
